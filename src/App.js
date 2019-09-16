9a2171071f6618ffcebb6fb583651aaf4539a2cc
malicha <maalicha.maalicha@gmail.com>
2019-09-16 10:26:13 +0700

    first apexcharts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ApexCharts from './charts'
+
+function App() {
+  return (
+    <div className="App">
+      <ApexCharts />
+    </div>
+  );
+}
+
+export default App;
